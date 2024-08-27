e26d6338e0269448f879d9b1a5c58e61e96bfb86
Biasiolo <biasiolorafael@gmail.com>
2024-08-27 00:45:08 -0300

    commit1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,25 +1,19 @@
-// Define a função globalmente
 function enviarWhatsApp() {
   var nome = document.getElementById("nome").value;
   var email = document.getElementById("email").value;
 
-  // Monta a mensagem com nome e e-mail
-  var mensagem = "Olá, gostaria de mais informações sobre o Niwa Clube de Pesca. Meu nome é " + encodeURIComponent(nome) + " e meu e-mail é " + encodeURIComponent(email) + ".";
 
-  // Link para o WhatsApp com a mensagem pré-definida
+  var mensagem = "Olá, gostaria de mais informações sobre o Niwa Clube de Pesca. Meu nome é " + encodeURIComponent(nome) + " e meu e-mail é " + encodeURIComponent(email) + ".";
   var linkWhatsApp = "https://api.whatsapp.com/send?phone=+5516997683413&text=" + mensagem;
 
-  // Abre o link do WhatsApp em uma nova aba
   window.open(linkWhatsApp, '_blank');
 
-  // Limpa o formulário
   document.getElementById("whatsappForm").reset();
 }
 
 $(document).ready(function() {
-  // Vincula a função ao clique do botão
   $("#whatsappForm").on("submit", function(event) {
-    event.preventDefault(); // Impede o envio do formulário
+    event.preventDefault(); 
     enviarWhatsApp();
   });
 });
